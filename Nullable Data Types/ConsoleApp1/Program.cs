1bfd58d6b21cf903b8ca16776db4faf8339e4fbf
anonym <mostaphabott@gmail.com>
2025-11-03 17:07:02 +0100

    nullable data types

---
+++ Nullable Data Types/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+class Entry2
+{
+
+    static void Main()
+    {
+        Nullable<int> ID = null; //Assign null value to integer
+        //or in short
+        int? ID2 = null;
+        if (!ID.HasValue)
+        {
+            Console.WriteLine("Id Is null");
+        }
+        if (ID == null)
+        {
+            Console.WriteLine("Id Is null");
+        }
+        int TempID = ID ?? 0; //assign 0 to temp if ID isnull otherwise affect the id value
+        Console.WriteLine(TempID);
+
+        string? Name = null;
+        Console.WriteLine("My name is "+Name?.ToLower()); //condition
+
+        DateTime? date = null;
+        Console.WriteLine(((DateTime)date).Day);
+
+    }
+}
