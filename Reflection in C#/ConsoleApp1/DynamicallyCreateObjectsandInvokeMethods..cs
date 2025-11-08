046bc9725e003cb237d526c75d26f45e9b9523d9
anonym <mostaphabott@gmail.com>
2025-11-08 09:14:21 +0100

    reorder files

--- Reflection in C#/ConsoleApp1/DynamicallyCreateObjectsandInvokeMethods..cs
+++ Reflection in C#/ConsoleApp1/DynamicallyCreateObjectsandInvokeMethods..cs
@@ -1,26 +1,95 @@
-﻿using System;
+﻿
+using System;
+using System.Reflection;
 
 namespace _Reflection
-{ 
-    class ClsType
+{
+    public class Person
     {
-        class test
+        private string FullName { set; get; } = "";
+        private int ID { set; get; }
+        public Person(string fullName, int iD)
         {
-            public string name = "";
+            FullName = fullName;
+            ID = iD;
         }
-        static void Main2()//change the name to Main if you want run this file as an entry
+        private void PrintInfo(DateTime dateTime)
         {
-            Type type = typeof(Type);
-            Console.WriteLine(type.Name);
-            Console.WriteLine(type.FullName);
-            Console.WriteLine(type.Assembly);
-            Console.WriteLine(type.IsValueType);
-            Console.WriteLine(type.IsClass);
-            Console.WriteLine(type.Attributes);
-            Console.WriteLine(type.BaseType);
-            Console.WriteLine(type.DeclaringType);
-            Console.WriteLine(type.IsInterface);
+            Console.WriteLine($"==============={dateTime.ToString()}====================");
+            Console.WriteLine("Name = " + FullName);
+            Console.WriteLine("ID   = " + ID);
+        }
+        private string InfoAsText()
+        {
+            return "Name = " + FullName + " ID   = " + ID + ".";
+        }
+    }
+    class Program
+    {
+        private static string Parameters(ParameterInfo[] parameterInfos)
+        {
+            if (parameterInfos.Length == 0)
+                return "()";
+            string line = "(";
+            foreach (ParameterInfo info in parameterInfos)
+            {
+                line += info.ParameterType + " " + info.Name + ")";
+            }
+            return line;
+        }
+        static void Main5()//change the name to Main if you want run this file as an entry
+        {
+            Type ClassType = typeof(Person);
+            Console.WriteLine("Properties :");
+            foreach (PropertyInfo property in ClassType.GetProperties((BindingFlags.NonPublic | BindingFlags.Instance)))
+            {
+                Console.WriteLine(property.Name + " " + property.PropertyType + " ");
+            }
+            Console.WriteLine("===================================");
+            Console.WriteLine("Methods :");
+            foreach (MethodInfo method in ClassType.GetMethods((BindingFlags.NonPublic | BindingFlags.Instance)))
+            {
+                Console.WriteLine(method.ReturnParameter + " " + method.Name + " " + Parameters(method.GetParameters()));
+            }
 
+            Console.WriteLine("===================================");
+            Console.WriteLine("Create Object :");
+
+            object? person = Activator.CreateInstance(ClassType, ["", 0]);
+            ClassType.GetProperty("FullName", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(person, "Mustapha Botte");
+            ClassType.GetProperty("ID", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(person, 500);
+
+            Console.WriteLine("ID   = " + ClassType.GetProperty("ID", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(person));
+            Console.WriteLine("Name = " + ClassType.GetProperty("FullName", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(person));
+
+            Console.WriteLine("===================================");
+            Console.WriteLine("Invode Methods :");
+
+            MethodInfo[] methodInfos = ClassType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (MethodInfo methodInfo in methodInfos)
+            {
+                if (methodInfo.IsSpecialName)
+                    continue;
+                if (methodInfo.ReturnType == typeof(void))
+                {
+                    if (methodInfo.GetParameters().Length == 0)
+                        methodInfo.Invoke(person, null);
+                    else
+                    {
+                        List<object> parameters = new List<object>();
+                        foreach (ParameterInfo parameterInfo in methodInfo.GetParameters())
+                        {
+                            parameters.Add(Activator.CreateInstance(parameterInfo.ParameterType));
+                        }
+                        methodInfo.Invoke(person, parameters.ToArray());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invoked function output = " + methodInfo.Invoke(person, null));
+                }
+            }
+            // All this in run time not compile time
         }
     }
 }
