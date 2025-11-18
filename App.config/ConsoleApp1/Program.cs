bd724ba23b1686c581689bec599c726288443c55
anonym <mostaphabott@gmail.com>
2025-11-18 12:40:41 +0100

    config file

---
+++ App.config/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionStringSettings ConnectiontoDb = ConfigurationManager.ConnectionStrings["connection"];
+
+
+
+            string projectname = ConfigurationManager.AppSettings["ApplicationName"] ?? "";
+            string version = ConfigurationManager.AppSettings["Version"] ?? "";
+
+            Console.WriteLine(ConnectiontoDb.Name);
+            Console.WriteLine(ConnectiontoDb.ConnectionString);
+            Console.WriteLine(ConnectiontoDb.ProviderName);
+            Console.WriteLine(ConnectiontoDb.Name);
+
+            Console.WriteLine(projectname);
+            Console.WriteLine(version);
+
+
+            //app.config after compile time moved to the bin folder and renamed to projectname.dll.config
+
+        }
+    }
+}
