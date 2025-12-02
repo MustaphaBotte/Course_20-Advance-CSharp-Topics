a92a437cca62afd7f21a694f0e0a58b783d98743
anonym <mostaphabott@gmail.com>
2025-12-02 21:38:18 +0100

    parallel class

---
+++ Asynchronous programming/Task Factory/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static string DownloadFile(string Path)
+        {
+            WebRequest httpClient = HttpWebRequest.Create(Path);
+            {
+                WebResponse response = httpClient.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        string content = reader.ReadToEnd();
+                      //  Console.WriteLine(content);
+                        return content;
+                    }
+                }
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            
+
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            string URL = "https://www.amazon.com";
+
+            TaskFactory<string> taskFactory = new TaskFactory<string>(tokenSource.Token,TaskCreationOptions.AttachedToParent,
+                TaskContinuationOptions.ExecuteSynchronously,TaskScheduler.Default);
+
+            Task<string> File1 = taskFactory.StartNew(() => DownloadFile(URL));
+            Task<string> File2 = taskFactory.StartNew(() => DownloadFile(URL));
+            Task<string> File3 = taskFactory.StartNew(() => DownloadFile(URL));
+            try
+            {
+                Task.WaitAll(File1, File2, File3);
+                Console.WriteLine(File1.Result.Length);
+            }
+            catch
+            {
+                tokenSource.Cancel(); // thiw will cancel them all in any error happend
+            }
+
+            Console.WriteLine("=================================================================================");
+    
+
+        }
+    }
+}
---
+++ Multithreading/Parallel Class/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static string DownloadFile(string Path)
+        {
+            WebRequest httpClient = HttpWebRequest.Create(Path);
+            {
+                WebResponse response = httpClient.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        string content = reader.ReadToEnd();
+                        Console.WriteLine(Task.CurrentId);
+                        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                        Console.WriteLine(Thread.CurrentThread.IsThreadPoolThread);
+                        Console.WriteLine("=========================================");
+                        return content;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 10,(i) =>{ DownloadFile("https://www.amazon.com"); });
+            // each time we are creating a new task that use some thread from thread pool
+
+            Console.WriteLine("******************************************************************************************");
+            string[] Urls = new string[] { "https://www.amazon.com", "https://www.amazon.com", "https://www.amazon.com" };
+            Parallel.ForEach(Urls, (url)=> DownloadFile(url));
+            // again fro each url we are creating a new task that use some thread from thread pool
+            Console.WriteLine("******************************************************************************************");
+
+            string Url = "https://www.google.com";
+            Parallel.Invoke(() => DownloadFile(Url), () => DownloadFile(Url), () => DownloadFile(Url));
+            // again fro each function call we are creating a new task that use some thread from thread pool
+            Console.WriteLine("******************************************************************************************");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+//The Parallel class in C# provides a simple and efficient way to implement parallelism in your applications.
+//It's particularly useful for scenarios where you need to perform operations concurrently across collections or
+//execute multiple independent actions simultaneously.
+//However, always consider thread safety and the nature of the tasks when using this class.
+
+//Parallel.For: Executes a for loop in which iterations may run in parallel.
+//Parallel.ForEach: Executes a foreach loop over any IEnumerable or IEnumerable<T> where iterations may run in parallel.
+//Parallel.Invoke: Executes each provided Action in parallel.
