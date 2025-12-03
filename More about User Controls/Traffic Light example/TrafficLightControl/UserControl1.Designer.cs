be39627bdf605d57f40bedcaf8d2b49e0649b81e
anonym <mostaphabott@gmail.com>
2025-12-03 23:49:06 +0100

    traffic lights mini project using threading (thread pool)

---
+++ More about User Controls/Traffic Light example/TrafficLightControl/UserControl1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TrafficLights
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TrafficLightPicture = new PictureBox();
+            Timinglabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)TrafficLightPicture).BeginInit();
+            SuspendLayout();
+            // 
+            // TrafficLightPicture
+            // 
+            TrafficLightPicture.Image = Properties.Resources.Red;
+            TrafficLightPicture.Location = new Point(3, 3);
+            TrafficLightPicture.Name = "TrafficLightPicture";
+            TrafficLightPicture.Size = new Size(185, 277);
+            TrafficLightPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+            TrafficLightPicture.TabIndex = 0;
+            TrafficLightPicture.TabStop = false;
+            // 
+            // Timinglabel
+            // 
+            Timinglabel.AutoSize = true;
+            Timinglabel.Font = new Font("Javanese Text", 18.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Timinglabel.Location = new Point(64, 283);
+            Timinglabel.Name = "Timinglabel";
+            Timinglabel.Size = new Size(39, 43);
+            Timinglabel.TabIndex = 1;
+            Timinglabel.Text = "";
+            Timinglabel.ForeColor = System.Drawing.Color.Yellow;
+
+             // 
+             // UserControl1
+             // 
+             AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(Timinglabel);
+            Controls.Add(TrafficLightPicture);
+            Name = "UserControl1";
+            Size = new Size(191, 326);
+            ((System.ComponentModel.ISupportInitialize)TrafficLightPicture).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox TrafficLightPicture;
+        private Label Timinglabel;
+    }
+}
---
+++ More about User Controls/Traffic Light example/TrafficLightControl/UserControl1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using static TrafficLights.UserControl1;
+
+namespace TrafficLights
+{
+  
+    public partial class UserControl1: UserControl
+    {
+        public event Action<LightColor> OnLightColorChanges = delegate { };
+        CancellationTokenSource tokensource = new CancellationTokenSource();
+        public enum EnColors { Red, Green, Orange };
+        public EnColors _CurrentLightColor = EnColors.Red;
+
+      
+
+        public class LightColor
+        {
+            public  EnColors Color = EnColors.Red;
+            public  int Timing = 0;
+            public LightColor(EnColors color)
+            {
+                this.Color = color;
+                if (this.Color == EnColors.Green)
+                    this.Timing = _GreenTime;
+
+                else if (this.Color == EnColors.Orange)
+                    this.Timing = _OrangeTime;
+
+                else if (this.Color == EnColors.Red)
+                    this.Timing = _RedTime;
+            }
+            public LightColor Next()
+            {
+                if(this.Color == EnColors.Red)
+                {
+                    return new LightColor(EnColors.Orange);
+                }
+                else if (this.Color == EnColors.Orange)
+                {
+                    return new LightColor(EnColors.Green);
+                }
+                else               
+                    return new LightColor(EnColors.Red);
+                
+            }
+        }
+        public EnColors Color 
+        {
+            set
+            {
+                this._CurrentLightColor = value;
+                if (_CurrentLightColor == EnColors.Green)
+                    this.TrafficLightPicture.Image = Properties.Resources.Green;
+
+                else if (_CurrentLightColor == EnColors.Red)
+                    this.TrafficLightPicture.Image = Properties.Resources.Red;
+
+                else if (_CurrentLightColor == EnColors.Orange)
+                    this.TrafficLightPicture.Image = Properties.Resources.Orange;        
+            }
+            get
+            {
+               return this._CurrentLightColor;
+            }
+        }
+
+
+        private static int _GreenTime = 6;
+        private static int _OrangeTime = 5;
+        private static int _RedTime = 9;
+
+        public int RedColorTime
+        {
+            set
+            {
+                _RedTime = value;
+            }
+            get
+            {
+              return _RedTime;
+            }
+        }
+        public int OrangeColorTime
+        {
+            set
+            {
+                _OrangeTime = value;
+            }
+            get
+            {
+                return _OrangeTime;
+            }
+        }
+        public int GreenColorTime
+        {
+            set
+            {
+                _GreenTime = value;
+            }
+            get
+            {
+                return _GreenTime;
+            }
+        }
+        public UserControl1()
+        {        
+            InitializeComponent();
+        }
+
+        private void ChangePicture()
+        {
+            if (_CurrentLightColor == EnColors.Green)
+                this.TrafficLightPicture.Image = Properties.Resources.Green;
+
+            else if (_CurrentLightColor == EnColors.Red)
+                this.TrafficLightPicture.Image = Properties.Resources.Red;
+
+            else if (_CurrentLightColor == EnColors.Orange)
+                this.TrafficLightPicture.Image = Properties.Resources.Orange;
+        }
+        private void DecrementTime(int seconds)
+        {
+            if (this.IsDisposed)
+                tokensource.Cancel();
+            for (int i = seconds; i > 0; i--)
+            {               
+                Thread.Sleep(1000);
+                this.Invoke(delegate () { this.Timinglabel.Text = i.ToString() + "s"; });              
+            }
+        }
+        private void HandleLightColorChanged(LightColor light)
+        {
+            Task.Run( ()=>
+            {
+                while (true)
+                {
+                    DecrementTime(light.Timing);
+                    light = light.Next();
+                    Color = light.Color;
+                    Task.Run(() => MessageBox.Show($"Current Light Color is {light.Color.ToString()} please wait {light.Timing}s"));
+                    
+                }
+            }, tokensource.Token);
+        }             
+        public void Start()
+        {
+            HandleLightColorChanged(new LightColor(_CurrentLightColor));
+        }
+    }
+}
