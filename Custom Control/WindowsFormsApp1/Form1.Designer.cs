95eca978374d6fa685329b6b08fb83d26440a42b
anonym <mostaphabott@gmail.com>
2025-12-04 20:07:59 +0100

    Custom Controls

---
+++ Custom Control/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxCustomControl1 = new WindowsFormsApp1.TextBoxCustomControl();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Font = new System.Drawing.Font("Constantia", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(162, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Custom Control Example";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(313, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxCustomControl1
+            // 
+            this.textBoxCustomControl1.Isrequired = false;
+            this.textBoxCustomControl1.Location = new System.Drawing.Point(297, 74);
+            this.textBoxCustomControl1.Name = "textBoxCustomControl1";
+            this.textBoxCustomControl1.Size = new System.Drawing.Size(153, 20);
+            this.textBoxCustomControl1.TabIndex = 1;
+            this.textBoxCustomControl1.Type = WindowsFormsApp1.TextBoxCustomControl.EnType.Numeric;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(775, 204);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxCustomControl1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private TextBoxCustomControl textBoxCustomControl1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Custom Control/WindowsFormsApp1/TextBoxCustomControl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class TextBoxCustomControl: TextBox
+    {
+        public TextBoxCustomControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+          //  base.OnPaint(pe);
+        }
+        public enum EnType { Text,Numeric,Email,Phone}
+        public EnType Type
+        {
+           set;
+           get;      
+        } = EnType.Text;
+        public bool Isrequired { set; get; } = false;
+
+        public bool IsValid()
+        {
+            if(Isrequired)
+            {
+                return (this.Text.Trim().Length > 0);
+            }
+            else if(!Isrequired && (this.Text.Trim().Length == 0))
+            {
+                return true;
+            }
+            // if not required and the user provide it (email or phone) we must validate it
+            if (this.Type == EnType.Numeric)
+            {
+               foreach(char c in Text)
+                {
+                    if (!char.IsDigit(c) && c != '.')
+                        return false;
+                }
+                return true;
+            }
+            else if (this.Type == EnType.Email)
+            {
+                return Regex.IsMatch(Text, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            }
+            else if (this.Type == EnType.Phone)
+            {
+                return Regex.IsMatch(Text, @"^(?:\+212)?\s?[5-7]\d{8}$");
+            }
+            return false;
+        }
+
+    }
+}
